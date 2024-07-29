54dec9c8b77bc4711610a5f33468aa18e825264e
Vteles96 <80376139+Vteles96@users.noreply.github.com>
2024-07-29 22:11:24 +0100

    29/07

--- 11-Cafeteria/gulpfile.js
+++ 11-Cafeteria/gulpfile.js
@@ -4,6 +4,8 @@ const { src, dest, watch, series, parallel} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer')
+const sourcemaps = require('gulp-sourcemaps');
+const cssnano = require('cssnano');
 
 //Imagenes
 const imagemin = require('gulp-imagemin');
@@ -12,13 +14,16 @@ const avif = require('gulp-avif');
 
 
 
+
 function css(done){
     //compila sass
     // pasos: 1- identificar archivo, 2- compilarlo, 3- guardar el .css
 
     src('src/scss/app.scss')
+        .pipe( sourcemaps.init() )
         .pipe( sass())
-        .pipe( postcss( [autoprefixer()] ))
+        .pipe( postcss( [autoprefixer(), cssnano()] ))
+        .pipe( sourcemaps.write('.'))
         .pipe( dest('build/css'))
     
     done();  
